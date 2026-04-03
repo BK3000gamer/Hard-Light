621cc64f05d99c7480ce5c203ce9155efb28b0af
melusi-ndebele <104642765@student.swin.edu.au>
2026-04-04 01:14:01 +1100

    -Added controlable camera with attatched flashlight -Camera ony moaves and light only turns on when mouse is pressed down to simulate pen on tablet -Flashlight angle and intensity adjustable with arrow keys

---
+++ Addons/proto_controller/proto_controller.gd
@@ -0,0 +1,94 @@
+extends CharacterBody3D
+
+@export_group("Speeds")
+## Look around rotation speed.
+@export var look_speed : float = 0.002
+
+@export_group("Input Actions")
+## Name of Input Action to Increase flashlight beam angle.
+@export var input_flashlight_beam_up : String = "flashlight_beam_up"
+## Name of Input Action to Decrease flashlight beam angle.
+@export var input_flashlight_beam_down : String = "flashlight_beam_down"
+## Name of Input Action to Increase flashlight intensity.
+@export var input_flashlight_intensity_up : String = "flashlight_intensity_up"
+## Name of Input Action to Decrease flashlight intensity.
+@export var input_flashlight_intensity_down : String = "flashlight_intensity_down"
+
+const FLASHLIGHT_MAX_BEAM_ANGLE = 35
+const FLASHLIGHT_MIN_BEAM_ANGLE = 20
+const FLASHLIGHT_MAX_INTENSITY = 15
+const FLASHLIGHT_MIN_INTENSITY = 3
+
+var mouse_captured : bool = false
+var look_rotation : Vector2
+
+## IMPORTANT REFERENCES
+@onready var head: Node3D = $Head
+@onready var collider: CollisionShape3D = $Collider
+@onready var flashlight : Flashlight = get_node("Head/Camera3D/Flashlight") as Flashlight
+
+func _ready() -> void:
+	check_input_mappings()
+	look_rotation.y = rotation.y
+	look_rotation.x = head.rotation.x
+
+func _unhandled_input(event: InputEvent) -> void:
+	# Mouse capturing
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		capture_mouse()
+	if Input.is_key_pressed(KEY_ESCAPE):
+		release_mouse()
+	
+	# Look around
+	if mouse_captured and event is InputEventMouseMotion:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			rotate_look(event.relative)
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed(input_flashlight_intensity_up) and flashlight.light_intensity < FLASHLIGHT_MAX_INTENSITY:
+		flashlight.light_intensity += delta * 20
+
+	if Input.is_action_pressed(input_flashlight_intensity_down) and flashlight.light_intensity > FLASHLIGHT_MIN_INTENSITY:
+		flashlight.light_intensity -= delta * 20
+
+	if Input.is_action_pressed(input_flashlight_beam_up) and flashlight.beam_angle < FLASHLIGHT_MAX_BEAM_ANGLE:
+		flashlight.beam_angle += 2
+
+	if Input.is_action_pressed(input_flashlight_beam_down) and flashlight.beam_angle > FLASHLIGHT_MIN_BEAM_ANGLE:
+		flashlight.beam_angle -= 2
+
+
+## Rotate us to look around.
+## Base of controller rotates around y (left/right). Head rotates around x (up/down).
+## Modifies look_rotation based on rot_input, then resets basis and rotates by look_rotation.
+func rotate_look(rot_input : Vector2):
+	look_rotation.x -= rot_input.y * look_speed
+	look_rotation.x = clamp(look_rotation.x, deg_to_rad(-85), deg_to_rad(85))
+	look_rotation.y -= rot_input.x * look_speed
+	transform.basis = Basis()
+	rotate_y(look_rotation.y)
+	head.transform.basis = Basis()
+	head.rotate_x(look_rotation.x)
+
+func capture_mouse():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	mouse_captured = true
+
+
+func release_mouse():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	mouse_captured = false
+
+
+## Checks if some Input Actions haven't been created.
+## Disables functionality accordingly.
+func check_input_mappings():
+	if not InputMap.has_action(input_flashlight_beam_up):
+		print("Warning: No input mapping for increasing flashlight beam angle. Action name: " + input_flashlight_beam_up)
+	if not InputMap.has_action(input_flashlight_beam_down):
+		print("Warning: No input mapping for decreasing flashlight beam angle. Action name: " + input_flashlight_beam_down)
+	if not InputMap.has_action(input_flashlight_intensity_up):
+		print("Warning: No input mapping for increasing flashlight intensity. Action name: " + input_flashlight_intensity_up)
+	if not InputMap.has_action(input_flashlight_intensity_down):
+		print("Warning: No input mapping for decreasing flashlight intensity. Action name: " + input_flashlight_intensity_down)
